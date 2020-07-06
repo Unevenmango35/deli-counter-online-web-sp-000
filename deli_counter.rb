e3e00045c7f29c7cb86b4ad86ccba5e2c4365b80
Rosie Choi <rosiechoimanso@gmail.com>
2020-07-06 17:59:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,4 +19,9 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
 end
 
-def 
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli.unshift}."
+end
